db6b3fac7e169afd42471402a14411255e3f6f91
wnmay <wanathachat@gmail.com>
2024-12-30 15:59:36 +0700

    feat: add GET api for getting product

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -27,3 +27,12 @@ export const deleteProduct = async (req,res)=>{
 		res.status(500).json({success:false, message: 'internal server error'})
 	}
 }
+
+export const getProduct = async (req,res)=>{
+	try {
+		const products = await Product.find();
+		res.status(200).json({ success: true, data: products });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "internal server Error" });
+	}
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { deleteProduct, postProducts } from '../controllers/products.controller.js'
+import { deleteProduct, getProduct, postProducts } from '../controllers/products.controller.js'
 
 const router = express.Router()
 
 router.post("/",postProducts)
 router.delete("/:id", deleteProduct)
+router.get('/',getProduct)
 
 export default router;
