f877cd700ce3bd8ae123ff37492d5460f0cf4ab4
wnmay <wanathachat@gmail.com>
2024-12-30 14:55:00 +0700

    feat: add POST api for adding product

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
-    res.send("hello")
+app.use(express.json())
+
+app.post("/api/products",async (req,res)=>{
+    const product = req.body;
+	if (!product.name || !product.price || !product.image ) {
+		return res.status(400).send('All fields are required.');
+	}
+	const addedProduct = new Product(product);
+	try {
+		await addedProduct.save();
+		res.status(201).json({success:true, data: addedProduct });
+	} catch (error) {
+		res.status(500).json({success:false, message: 'Internal server error'})
+	}
 })
 
 app.listen(PORT, () => {
