36a07446a6562951b0b9b1538dfa9f05538791cb
wnmay <wanathachat@gmail.com>
2024-12-30 16:07:49 +0700

    feat: add PUT api for updating product

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -36,3 +36,18 @@ export const getProduct = async (req,res)=>{
 		res.status(500).json({ success: false, message: "internal server Error" });
 	}
 }
+
+export const updateProduct = async (req, res) => {
+	const { id } = req.params;
+	const product = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ success: false, message: "Product not found" });
+	}
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+		res.status(200).json({ success: true, data: updatedProduct });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "internal server error" });
+	}
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { deleteProduct, getProduct, postProducts } from '../controllers/products.controller.js'
+import { deleteProduct, getProduct, postProducts, updateProduct } from '../controllers/products.controller.js'
 
 const router = express.Router()
 
 router.post("/",postProducts)
 router.delete("/:id", deleteProduct)
-router.get('/',getProduct)
+router.get("/",getProduct)
+router.put("/:id",updateProduct)
 
 export default router;
