e3063371cdb12842817cd74ff1595459e6e534cf
wnmay <wanathachat@gmail.com>
2025-01-07 23:27:04 +0700

    feat: about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,12 @@
 const About = ()=>{
     return(
         <>
-        <div>about</div>
+        <div className="flex flex-col justify-center item-center m-12 gap-4">
+        <div className="text-primary text-center text-4xl font-bold">About Online Store</div>
+        <div className="text-xl">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This project is a full-stack application built using the MERN stack for educational purposes. If you have any recommendations or notice any issues, please feel free to reach out!</div>
+        <div className="text-xl">My github: <a href="https://github.com/wnmay" className="underline text-primary font-bold hover:text-secondary">Click!</a></div>
+
+        </div>
         </>
     )
 }
